56173a00df83609628bc456168755f7aaa3208f2
taemon1337 <taemon1337@gmail.com>
2024-09-21 00:41:57 +0000

    added main page

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-
+<div class="responsive">
+    <div>
+        <aside class="left medium-width medium-height padding">
+            <img class="round responsive" src="/birthday-original.jpg">
+        </aside>
+        <h5><button class="primary chip medium">Step 1</button> Login to your Photos</h5>
+        <p>Currently only support for Google Photos.</p>
+        <br/>
+        <h5><button class="primary chip medium">Step 2</button> Select Background image</h5>
+        <p> Select your main mosaic background image.</p>
+    </div>
+    <div>
+        <aside class="left medium-width medium-height padding">
+            <img class="round responsive" src="/birthday-grid.jpg">
+        </aside>
+        <h5><button class="primary chip medium">Step 3</button> Select images</h5>
+        <p>Pick as many images as you want to fill the mosaic with, 100s or even 1000s.</p>
+    </div>
+    <div>
+        <aside class="left medium-width medium-height padding">
+            <img class="round responsive" src="/birthday-mosaic.jpg">
+        </aside>
+        <h5><button class="primary chip medium">Step 4</button>Generate Mosaic</h5>
+        <p>Mosaic will be combination of background image plus selected images overlayed on top.</p>
+    </div>
+    <div>
+        <aside class="left medium-width medium-height padding">
+            <img class="round responsive" src="/birthday-mosaic-light.jpg">
+        </aside>
+        <h5><button class="primary chip medium">Step 5</button>Adjust Mosaic</h5>
+        <p>Adjust and filter your mosaic to get just the right effect.</p>
+        <br/>
+        <h5><button class="primary chip medium">Step 6</button>Save or Download Mosaic</h5>
+        <p>Mosaic can be downloaded in poster scale sizes for high quality photo prints.</p>
+    </div>
+</div>
