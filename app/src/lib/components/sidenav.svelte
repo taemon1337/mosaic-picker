5dba7accc6b38a6381ceec46eb284c46bda520d2
taemon1337 <taemon1337@gmail.com>
2024-09-23 19:39:33 +0000

    trying google login

--- app/src/lib/components/sidenav.svelte
+++ app/src/lib/components/sidenav.svelte
@@ -1,29 +1,22 @@
 <script>
-    import { isLoggedIn, user } from '$lib/stores/auth.js';
+    import SignIn from '$lib/components/signin.svelte';
+    import {page} from "$app/stores";
 
-    let loggedIn;
-    let currentUser;
-
-    // Subscribe to the auth store
-    $: loggedIn = $isLoggedIn;
-    $: currentUser = $user;
+    let loggedIn = $page.data.session
 </script>
 <div>
     <nav class="left drawer l">
     <header>
       <nav>
-        <img src="%sveltekit.assets%/favicon.png" class="circle" alt="sveltekit">
-        <h6>Mosaic</h6>
+        <img src="/favicon.png" class="circle" alt="sveltekit">
+        <h6>photoMosaic</h6>
       </nav>
     </header>
     <a href="/">
       <i>home</i>
       <div>Home</div>
     </a>
-    <a href="/login">
-      <i>account_circle</i>
-      <div>Login</div>
-    </a>
+    {#if loggedIn}
     <a href="/studio">
       <i>create</i>
       <div>Mosaic Studio</div>
@@ -32,6 +25,18 @@
       <i>search</i>
       <div>My Mosaics</div>
     </a>
+    {:else}
+    <a href="/login">
+      <i>account_circle</i>
+      <div>
+        <SignIn />
+      </div>
+    </a>
+    <a href="/studio">
+      <i>create</i>
+      <div>Mosaic Studio</div>
+    </a>
+    {/if}
     <div class="divider"></div>
     <a href="/gallery">
       <i>public</i>
---
+++ app/src/lib/components/signin.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { decodeJwtResponse, onGoogleScriptLoad } from '$lib/stores/auth.ts';
+    
+    onMount(() => {
+      onGoogleScriptLoad(decodeJwtResponse);
+    });
+  </script>
+  
+  <div id="googleSignIn"></div>
